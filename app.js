bfbdd3adff447bd15c5c7d020c5a4bf337b71946
test <apyhajustbepostifa@gmail.com>
2022-10-28 12:29:06 +0530

    Level4 Encryption Authentication Implementation

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ import ejs from "ejs";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import encrypt from "mongoose-encryption";
+import md5 from "md5";
+import bcrypt from "bcrypt";
+
+const saltRounds = 10;
 
 const app = express();
 dotenv.config();
@@ -11,16 +15,16 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
-mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
+mongoose.connect("mongodb://localhost:27017/usersDB",{useNewUrlParser:true});
 
 const userSchema = new mongoose.Schema({
     email:String,
     password:String
 });
 
-const secret = process.env.SECRET;
+//const secret = process.env.SECRET;
 
-userSchema.plugin(encrypt, {secret:secret, encryptedFields: ['password'] });
+//userSchema.plugin(encrypt, {secret:secret, encryptedFields: ['password'] });
 
 const User=new mongoose.model("User",userSchema);
 
@@ -35,35 +39,44 @@ app.get("/register", (req,res) => {
 })
 
 app.post("/register", (req,res) => {
-    const newUser = new User({
-        email:req.body.username,
-        password:req.body.password
-    })
-    try {
-        newUser.save();
-        res.render("secrets");
-    }
-    catch(error) {
-        console.log(error);
-    }
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email:req.body.username,
+            //password:md5(req.body.password)
+            password:hash
+        })
+        try {
+            newUser.save();
+            res.render("secrets");
+        }
+        catch(error) {
+            console.log(error);
+        }
+    });
 })
 
 app.post("/login", (req,res) => {
     const username = req.body.username;
+    //const password = md5(req.body.password);
     const password = req.body.password;
-
-    User.findOne({email:username}, (err,result) => {
+    
+    User.findOne({email:username}, (err,foundUser) => {
         if(err) {
             console.log(err);
         }
         else {
-            if(result) {
-                if(result.password === password) {
-                    res.render("secrets");
-                }
-                else {
-                    res.write("Login failed");
-                }
+            if(foundUser) {
+                bcrypt.compare(password, foundUser.password, (err, result) => {
+                    if(result === true)
+                        res.render("secrets");
+                })
+            }
+            //if(result.password === password) {
+                //res.render("secrets");
+            //}
+            else {
+                res.write("Login failed");
             }
         }
     })
